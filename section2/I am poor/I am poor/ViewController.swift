581523c33c5f1bc42305b626ce2e18c741983b85
Justin <jelias153@g.rwu.edu>
2021-09-25 20:41:22 -0500

    I am poor - section 2
    I am poor - section 2

---
+++ section2/I am poor/I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Justin Elias on 9/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
